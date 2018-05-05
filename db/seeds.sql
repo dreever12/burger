ad630b72855d14a65f4f5ea732c934fdc7d4f3c6
Dennyela Reever <dennyela.reever@gmail.com>
2018-05-04 23:50:55 -0400

    hmk

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheesburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('South West Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Beeler Burger', true);
+
